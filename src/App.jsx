1a7a96e99e18fcbb4127a71c088db9e77aadca3a
D3NMOH <d3nmoh@gmail.com>
2024-02-01 12:41:32 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import "./App.css";
-import questions from "./data/page0.json";
+import Questions from "./components/Questions";
+import WelcomePage from "./components/WelcomePage";
+import Results from "./components/Results";
+import Ads from "./components/Ads";
 
 function App() {
-  const [page, setPage] = useState(0);
+  // const [page, setPage] = useState(0);
 
-  return <></>;
+  // const handleSwitchPage = (newPage) => {
+  //   setPage(newPage);
+  // };
+
+  return (
+    <>
+      <div className="App">
+        <h1>Hello world</h1>
+
+        <WelcomePage />
+        <Questions />
+        <Ads />
+        <Results />
+      </div>
+    </>
+  );
 }
 
 export default App;
