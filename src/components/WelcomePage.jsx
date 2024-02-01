76a271f2c26e3f1615635a75b86507a9a0121bab
Svitlana Bichurina <itservicedevmy@gmail.com>
2024-02-01 15:06:23 +0100

    workinprogress

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,3 +1,9 @@
+import styles from "./WelcomePage.module.css";
+
 export default function WelcomePage() {
-  return <div>WelcomePage</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.greetings}>Welcome!</h1>
+    </div>
+  );
 }
