686421414ee798aa061d52091bd921acf7d66344
D3NMOH <d3nmoh@gmail.com>
2024-02-02 12:25:20 +0100

    margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Questions from "./components/Questions";
 import WelcomePage from "./components/WelcomePage";
@@ -6,20 +6,42 @@ import Results from "./components/Results";
 import Ads from "./components/Ads";
 
 function App() {
-  // const [page, setPage] = useState(0);
+  const [page, setPage] = useState("welcome");
 
-  // const handleSwitchPage = (newPage) => {
-  //   setPage(newPage);
-  // };
+  const handleSwitchPage = (newPage) => {
+    setPage(newPage);
+  };
+
+  const showPage = () => {
+    switch (page) {
+      case "welcome":
+        return <WelcomePage />;
+      case "questions":
+        return <Questions />;
+      case "ads":
+        return <Ads />;
+      case "results":
+        return <Results />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <>
       <div className="app">
         <h1>Quiz app</h1>
-        <WelcomePage />
-        <Questions />
-        <Ads />
-        <Results />
+        <div className="switcher">
+          <button onClick={() => handleSwitchPage("welcome")}>
+            Welcome Page
+          </button>
+          <button onClick={() => handleSwitchPage("questions")}>
+            Questions
+          </button>
+          <button onClick={() => handleSwitchPage("ads")}>Ads</button>
+          <button onClick={() => handleSwitchPage("results")}>Results</button>
+        </div>
+        {showPage()}
       </div>
     </>
   );
