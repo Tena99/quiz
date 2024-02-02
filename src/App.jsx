ab477097e1a07908b2d75c481d23913991621bca
Anastasiia Volvis <stacyvolvis@gmail.com>
2024-02-02 12:29:23 +0100

    Add question switch logic

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <div className="app">
-        <h1>Hello world</h1>
+        <h1>Quiz</h1>
         {/* <WelcomePage /> */}
         <Questions />
         {/* <Ads />
