8b417b77c97f97895334b5527a3f474672130110
Anastasiia Volvis <stacyvolvis@gmail.com>
2024-02-07 13:07:23 +0100

    changed li rendering, removed prev btn

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styles from "./Questions.module.css";
 import questions from "../data/questions.json";
 
+const gridStyles = ["a", "b", "c", "d"];
+
 export default function Questions({
   onGoToPreviousPage,
   onGoToNextPage,
@@ -21,13 +23,6 @@ export default function Questions({
     }
   }
 
-  function prevQuestion() {
-    if (questionIndex > 0) {
-      setQuestionIndex(questionIndex - 1);
-      setCurrentAnswer(null);
-    }
-  }
-
   function chooseAnswer(answerId) {
     if (currentAnswer === null) {
       setCurrentAnswer(answerId);
@@ -39,77 +34,34 @@ export default function Questions({
       <progress
         value={`${questionIndex * 0.25}`}
         className={styles["progressBar"]}
-      >
-        <h4>You have answered {questionIndex + 1} from 5 questions</h4>
-      </progress>
+      ></progress>
 
       <h3> {currentQuestion.question}</h3>
 
       <div className={styles["answers_container"]}>
         <ul className={styles.qlist}>
-          <li
-            disabled={currentAnswer !== null}
-            className={`${styles.a} ${
-              currentAnswer === `${currentQuestion.answers[0]}`
-                ? currentAnswer === questions[questionIndex].correctAnswer
-                  ? styles["correct_answer"]
-                  : styles["wrong_answer"]
-                : undefined
-            }`}
-            onClick={() => chooseAnswer(currentQuestion.answers[0])}
-          >
-            <strong>A: </strong> {currentQuestion.answers[0]}
-          </li>
-          <li
-            disabled={currentAnswer !== null}
-            className={`${styles.b} ${
-              currentAnswer === `${currentQuestion.answers[1]}`
-                ? currentAnswer === questions[questionIndex].correctAnswer
-                  ? styles["correct_answer"]
-                  : styles["wrong_answer"]
-                : undefined
-            }`}
-            onClick={() => chooseAnswer(currentQuestion.answers[1])}
-          >
-            <strong>B: </strong> {currentQuestion.answers[1]}
-          </li>
-          <li
-            disabled={currentAnswer !== null}
-            className={`${styles.c} ${
-              currentAnswer === `${currentQuestion.answers[2]}`
-                ? currentAnswer === questions[questionIndex].correctAnswer
-                  ? styles["correct_answer"]
-                  : styles["wrong_answer"]
-                : undefined
-            }`}
-            onClick={() => chooseAnswer(currentQuestion.answers[2])}
-          >
-            <strong>C: </strong> {currentQuestion.answers[2]}
-          </li>
-          <li
-            disabled={currentAnswer !== null}
-            className={`${styles.d} ${
-              currentAnswer === `${currentQuestion.answers[3]}`
-                ? currentAnswer === questions[questionIndex].correctAnswer
-                  ? styles["correct_answer"]
-                  : styles["wrong_answer"]
-                : undefined
-            }`}
-            onClick={() => chooseAnswer(currentQuestion.answers[3])}
-          >
-            <strong>D: </strong> {currentQuestion.answers[3]}
-          </li>
+          {currentQuestion.answers.map((answer, index) => {
+            return (
+              <li
+                disabled={currentAnswer !== null}
+                className={`${styles[gridStyles[index]]} ${
+                  currentAnswer === `${answer}`
+                    ? currentAnswer === questions[questionIndex].correctAnswer
+                      ? styles["correct_answer"]
+                      : styles["wrong_answer"]
+                    : undefined
+                }`}
+                onClick={() => chooseAnswer(answer)}
+              >
+                <strong>{gridStyles[index].toLocaleUpperCase()}:</strong>
+                {answer}
+              </li>
+            );
+          })}
         </ul>
       </div>
 
       <div className={styles["questions_buttons"]}>
-        <button
-          onClick={
-            currentQuestion === questions[0] ? onGoToPreviousPage : prevQuestion
-          }
-        >
-          <i className="fa-solid fa-angle-left"></i>
-        </button>
         <button
           onClick={
             currentQuestion === questions[4]
@@ -124,6 +76,7 @@ export default function Questions({
               : nextQuestion
           }
         >
+          Next question
           <i className="fa-solid fa-chevron-right"></i>
         </button>
       </div>
