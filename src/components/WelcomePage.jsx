f374ff4c86cac8717e6ff36be9991fe48f7cccd1
D3NMOH <d3nmoh@gmail.com>
2024-02-07 16:55:06 +0100

    cosmetic + user profile rework + grid optimization

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -4,23 +4,21 @@ import welcomeImage from "../assets/welcome.jpg";
 
 function WelcomePage({ goToNextPage }) {
   return (
-    <div>
-      <div className={styles.container}>
-        <h2>Willkommen zur Quiz-App!</h2>
-        <p>
-          Hier können Sie Ihre Programmierkenntnisse überprüfen. Klicken Sie
-          einfach auf die Schaltfläche unten, um zu beginnen!
-        </p>
+    <div className={styles.container}>
+      <h2>Willkommen zur Quiz-App!</h2>
+      <p>
+        Hier können Sie Ihre Programmierkenntnisse überprüfen. Klicken Sie
+        einfach auf die Schaltfläche unten, um zu beginnen!
+      </p>
 
-        <img
-          src={welcomeImage}
-          className={styles.welcomeImage}
-          alt="welcome image"
-        />
-        <button className={styles.welcomebutton} onClick={goToNextPage}>
-          START
-        </button>
-      </div>
+      <img
+        src={welcomeImage}
+        className={styles.welcomeImage}
+        alt="welcome image"
+      />
+      <button className={styles.welcomebutton} onClick={goToNextPage}>
+        START
+      </button>
     </div>
   );
 }
